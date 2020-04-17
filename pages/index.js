aa0545ddb022ecf680b4758b005574d940255fbb
davidroman-hub <jobroman83@gmail.com>
2020-04-17 20:06:39 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// function Home() {
+//     return <div>Welcome to Next.js!</div>
+//   }
+  
+//   export default Home
+  
+
+const Home  = () => {
+    return (<div>Hola next!</div>)
+}
+
+export default Home
