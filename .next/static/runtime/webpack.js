77236730a8844e25f622bb90b5c66d1e0ae2ee35
davidroman-hub <jobroman83@gmail.com>
2020-04-18 00:33:04 +0100

    adding register form with handle change and handle submit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "362a3bf092c1fff1279a";
+/******/ 	var hotCurrentHash = "e9bb8456556e11cd44b2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,67 @@
+
+import {useState} from 'react'
 import Layout from '../components/Layout'
 
 const Register  = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email:'',
+        password: '',
+        error:'',
+        success:'',
+        buttonText: 'Registrar',        
+    })
+
+
+    // destructure
+
+    const {name, email, password, error, success, buttonText} = state
+
+
+
+const handleChange = name => e => {
+    setState({...state,[name]: e.target.value,error:'', success:'', buttonText:'Registro' })
+}
+
+const handleSubmit = e=> {
+    e.preventDefault();
+    console.table({name, email, password})
+}
+
+
+    
+    const registerForm = () => (
+        <form onSubmit={handleSubmit} >
+            <div className="form-group">
+                <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Escribe tu nombre"/>
+            </div>
+            <div className="form-group">
+                <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Escribe tu E-mail"/>
+            </div>
+            <div className="form-group">
+                <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Tu Contraseña"/>
+            </div>
+            <div className="form-group">
+    <button className="btn btn-outline-warning">{buttonText}</button>
+            </div>
+        </form>
+    )
+    
+    
+    
     return (
     
-    <Layout>register page</Layout>
+    <Layout>
+        <div className="text-center col-md-6 offset-md-3">
+            <h2>Registro</h2>
+            <br/>
+            {registerForm()}
+            <hr/>
+            {/* {JSON.stringify({state})} */}
+
+        </div>
+    </Layout>
     
     )
 }
